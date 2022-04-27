63c5d6d403f2f2ac05963d9e7c1b992df4d4acf6
Fillip Hudson <Filliphudson222@gmail.com>
2022-04-27 20:31:17 -0300

    Editado

--- JavaScript/Lista de compras/script.js
+++ JavaScript/Lista de compras/script.js
@@ -1,14 +1,16 @@
 const addItens = document.querySelector('#itensNovos')
-const addPreco = document.querySelector('#itensNovosValor')
+const addPreco = document.querySelector('#itensPreco')
 const botaoSalvar = document.querySelector('#salvar')
-const itensAdicionados = document.querySelector('.adicionados')
+const itensAdicionados = document.querySelector('.itens-adicionados')
 const total = document.querySelector('.total')
-
 
 botaoSalvar.addEventListener('click', adicionar)
 
+var itens = []
+excluirItem() 
+
 function adicionar(){
-    var itens = []
+   
 
     itens.push({
         nome: addItens.value,
@@ -16,16 +18,50 @@ function adicionar(){
     })
 
     itensAdicionados.innerHTML = ""
+    let soma = 0
+    let itensTotal = 0
 
-    itens.map(function(val){   
-    itensAdicionados.innerHTML +=`
-    <h3>`+val.nome+`</h3>   
-    <h3>`+val.preco+`</h3>
+    itens.map(function(val){  
+        soma += parseFloat(val.preco)
+        itensTotal += 1
+        itensAdicionados.innerHTML +=`
+        <div class="adicionados">
+            <h3>`+val.nome+`</h3>   
+            <h3>`+val.preco+`</h3>
+            <div class="botoes"> 
+            <img src="/JavaScript/Lista de compras/img/edit.png" alt="">
+            <img src="/JavaScript/Lista de compras/img/delete.png" alt="" name="delete">
+            </div>
+        </div>
+
+  ` 
+
+      total.innerHTML = `
+      <h3> Total de Itens: ${itensTotal}</h3>
+      <h3> Valor Total: R$ ${soma}</h3>`
+
+
+        
+        
+    }) 
+}
+
+function excluirItem(){
+
+        
+}
+
+function limpar(){
+    itensAdicionados.innerHTML = `
+    <div class="adicionados">
+        <h3></h3>   
+        <h3>R$</h3>
     <div class="botoes"> 
-    <img src="/JavaScript/Lista de compras/img/edit.png" alt="">
-    <img src="/JavaScript/Lista de compras/img/delete.png" alt=""></div>
-    </br>`
-    })
+        <img src="/JavaScript/Lista de compras/img/edit.png" alt="">
+        <img src="/JavaScript/Lista de compras/img/delete.png" alt="">
+    </div>`
+    itens = []
 
+    total.innerHTML = ""
 
 }
