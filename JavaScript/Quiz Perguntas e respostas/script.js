38d88215a8a20ffad0021e96769d496d5d5bdc78
Fillip Hudson <Filliphudson222@gmail.com>
2022-05-12 16:02:54 -0300

    Passamos o quiz para cá

--- JavaScript/Quiz Perguntas e respostas/script.js
+++ JavaScript/Quiz Perguntas e respostas/script.js
@@ -1,49 +1,113 @@
+        // Chamando todas as váriaveis que podem ficar pra fora //
+
+// chamando a div que vai encapsular o resultado-quiz
 const resultado = document.querySelector('.resultado-do-quiz') 
-const labelsVDD = document.querySelectorAll('#label') // Chamando todas as labels do quiz.
+// Aqui as variáveis que fazem parte do resultado
+var questoes = 0
+var acertos = 0
 
-function finalizar(){ // Quando clicar em finalizar, acontece toda essa função.
-    var inputs = document.querySelector('input[type="radio"]:checked') // Chamando todos os inputs, e verificando se algum deles está clicado, com o :checked
-    var contato = document.querySelector('.contato') // Apenas para colocar a div no lugar certo
+// Chamando todas as labels do quiz, vamos acessar usando o [].
+const labelsVDD = document.querySelectorAll('#label')
 
-    if(inputs == null)
-    { // Verificando se algum input está clicado, se não, não continua o cod.
+// 2 variáveis de contato, apenas para poder encaixar a div no lugar certo
+var contato = document.querySelector('.contato')
+var contato1 = document.querySelector('.contato1')
+
+
+        // Elementos criados como feedback para cada questão //
+
+// Primeira pergunta 
+var div = document.createElement('div') 
+var div1 = document.createElement('div') 
+var div2 = document.createElement('div') 
+var div3 = document.createElement('div')
+var div4 = document.createElement('div') 
+// p´s
+var p = document.createElement('p') 
+var p1 = document.createElement('p')
+var p2 = document.createElement('p')
+var p3 = document.createElement('p')
+var p4 = document.createElement('p')
+// Classes pro CSS
+div.classList.add('div-1') 
+div1.classList.add('div-2')
+div2.classList.add('div-3')
+div3.classList.add('div-4')
+div4.classList.add('div-5')
+
+
+// Cópia para a pergunta 2, e assim seguiria se tivesse mais perguntas
+var div5 = document.createElement('div') 
+var div6 = document.createElement('div') 
+var div7 = document.createElement('div') 
+var div8 = document.createElement('div') 
+var div9 = document.createElement('div') 
+// p´s
+var p5 = document.createElement('p')
+var p6 = document.createElement('p')
+var p7 = document.createElement('p')
+var p8 = document.createElement('p')
+var p9 = document.createElement('p')
+// Classes
+div5.classList.add('div-6')
+div6.classList.add('div-7')
+div7.classList.add('div-8')
+div8.classList.add('div-9')
+div9.classList.add('div-10')
+
+
+            // Função de click no finalizar o quiz //
+function finalizar()
+{ 
+
+        // Chamando os inputs e verificando se estão clicados // 
+    var inputsPergunta = document.querySelector('.input-pergunta:checked')
+    var inputsPergunta1 = document.querySelector('.input-pergunta1:checked')
+    
+                // PRIMEIRA QUESTÃO DO CÓD //   
+
+        // Verificando se tem algum input que não está clicado //
+    if(inputsPergunta == null || inputsPergunta1 == null)
+    { 
         alert('É preciso preencher alguma pergunta para poder finalizar!')
     }
+
+            // Se todos estiverem clicados, segue o cód //
     else
-    { // Caso esteja clicado, roda todo esse cód:
+    { 
+        // Verificando se o input tem o valor errado de cada input //
+        if(inputsPergunta.value ==  'errado' || inputsPergunta.value ==  'errado1' || inputsPergunta.value ==  'errado2' || inputsPergunta.value ==  'errado3')
+        { 
+// Veio pra cá, significa que foi errada, então adiciona mais 1 questão. 
+            questoes += 1
 
-        var div = document.createElement('div') // Criando divs de feedback para a pessoa
-        var div1 = document.createElement('div') // ou seja, porque está certo, porque está
-        var div2 = document.createElement('div') // errado.
-        var div3 = document.createElement('div')
-        var div4 = document.createElement('div') 
-            
-        div.classList.add('div-1') // add uma class para cada, para poder estilizar no CSS
-        div1.classList.add('div-2')
-        div2.classList.add('div-3')
-        div3.classList.add('div-4')
-        div4.classList.add('div-5')
 
-        var p = document.createElement('p') // Criando os P´s que vão dar o feedback
-        var p1 = document.createElement('p')
-        var p2 = document.createElement('p')
-        var p3 = document.createElement('p')
-        var p4 = document.createElement('p')
-
-        if(inputs.value ==  'errado' || inputs.value ==  'errado1' || inputs.value ==  'errado2' || inputs.value ==  'errado3')
-        { // Verificando se o input é errado, coloquei cada um dos valores errado...
+        // Loops pra mostrar as cores vermelhas das perguntas erradas //
             for(var label of labelsVDD)
-            { // Aqui eu criei um loop que mostras as classes, e dai mostra as cores, se é 
-                label.classList.add('show') // certo ou não... no caso vermelho errado.
+            { 
+                label.classList.add('show')
             }   
-            labelsVDD[1].classList.add('show2') // e nesse aqui solitário que é apenas o verde.
+            // As duas labels certas, ou seja, verdes //
+            labelsVDD[1].classList.add('show2')
+            labelsVDD[7].classList.add('show2') 
        
-            if(inputs.value == 'errado')
-            { // Se for "errado" ou seja, clicou no primeiro, vai acontecer isso:
 
+            // Agora vai em cada uma das erradas e verifica //
+
+            if(inputsPergunta.value == 'errado')
+            { 
+
+            // Aqui ocorre o mesmo padrão para todas as questões //
+
+                // Label 1, insere a div antes de label 2
                 labelsVDD[0].parentNode.insertBefore(div, labelsVDD[1]) 
-                div.append(p) // chamando a label 1, e inserindo a div antes da label 2
-                p.innerText = `a Você errou!!!`// e esse texto, segue igual para todas as outras...
+                // põe o p dentro da div
+                div.append(p) 
+                // adiciona o texto do porque está errada
+                p.innerText = `Você errou!`
+
+
+                // Assim segue para todos as outras questões //
 
                 labelsVDD[2].parentNode.insertBefore(div1, labelsVDD[3])
                 div1.append(p1)
@@ -61,13 +125,10 @@ function finalizar(){ // Quando clicar em finalizar, acontece toda essa função
                 div4.append(p4)
                 
                 p4.innerText = `Essa era a resposta certa, porque...  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quisquam ut illum. Reprehenderit ea vero, non delectus eius voluptatum ipsum, est natus ipsam eos laborum quia maiores vel dolores aspernatur.`
-
-                resultado.innerHTML = `<h3>Você errou 1 de 1 questão.</h3>`
             }
 
-            else if(inputs.value == 'errado1')
-            { // Se for o errado 1, vai acontecer isso...
-    
+            else if(inputsPergunta.value == 'errado1')
+            {
                 labelsVDD[0].parentNode.insertBefore(div, labelsVDD[1])
                 div.append(p)
                 p.innerText = `Essa também não está certa por causa disso disso e disso...`
@@ -88,10 +149,10 @@ function finalizar(){ // Quando clicar em finalizar, acontece toda essa função
                 div4.append(p4)
                 p4.innerText = `Essa era a resposta certa, porque...  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quisquam ut illum. Reprehenderit ea vero, non delectus eius voluptatum ipsum, est natus ipsam eos laborum quia maiores vel dolores aspernatur.`
 
-                resultado.innerHTML = `<h3>Você errou 1 de 1 questão.</h3>`
+                
             }
 
-            else if(inputs.value == 'errado2'){
+            else if(inputsPergunta.value == 'errado2'){
     
                 labelsVDD[0].parentNode.insertBefore(div, labelsVDD[1])
                 div.append(p)
@@ -113,10 +174,10 @@ function finalizar(){ // Quando clicar em finalizar, acontece toda essa função
                 div4.append(p4)
                 p4.innerText = `Essa era a resposta certa, porque...  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quisquam ut illum. Reprehenderit ea vero, non delectus eius voluptatum ipsum, est natus ipsam eos laborum quia maiores vel dolores aspernatur.`
 
-                resultado.innerHTML = `<h3>Você errou 1 de 1 questão.</h3>`
+                
             }
 
-            else if(inputs.value == 'errado3')
+            else if(inputsPergunta.value == 'errado3')
             {
                 labelsVDD[0].parentNode.insertBefore(div, labelsVDD[1])
                 div.append(p)
@@ -138,21 +199,30 @@ function finalizar(){ // Quando clicar em finalizar, acontece toda essa função
                 div4.append(p4)
                 p4.innerText = `Essa era a resposta certa, porque...  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quisquam ut illum. Reprehenderit ea vero, non delectus eius voluptatum ipsum, est natus ipsam eos laborum quia maiores vel dolores aspernatur.`
 
-                resultado.innerHTML = `<h3>Você errou 1 de 1 questão.</h3>`
+   
             }
+            
         }
 
+        // Se não tem o valor errado, o cód vem pra cá //
+
         else
-        { // Aqui é apenas a opção quando a opção for verdadeira, ou seja, não for errada.
+        {
+            // refazendo o loop que deixa vermelho os errados //
+
             for(var label of labelsVDD){
-                label.classList.add('show') // refazendo aqui, porque essa é quando a pessoa acerta.
+                label.classList.add('show') 
                 }
+                // Deixando verde...
+                labelsVDD[1].classList.add('show2')
+                labelsVDD[7].classList.add('show2')    
 
-                labelsVDD[1].classList.add('show2') // copia..
+                // aumentando em 1 o número de questões
+                questoes += 1
+                // e agora sim somando 1 o número de acertos.
+                acertos += 1
 
-                labelsVDD[1].parentNode.insertBefore(div4, labelsVDD[2])// mesma lógica dos anteriores
-                div4.append(p4)
-                p4.innerText = `Parabéns você acertou!, Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quisquam ut illum. Reprehenderit ea vero, non delectus eius voluptatum ipsum, est natus ipsam eos laborum quia maiores vel dolores aspernatur.`
+                          // Mesma coisa... //
 
                 labelsVDD[0].parentNode.insertBefore(div, labelsVDD[1])
                 div.append(p)
@@ -170,8 +240,161 @@ function finalizar(){ // Quando clicar em finalizar, acontece toda essa função
                 div3.append(p3)
                 p3.innerText = `Essa resposta é errada por causa disso disso e disso...`
 
-                resultado.innerHTML = `<h3>Você acertou 1 de 1 questão.</h3>`
+                labelsVDD[1].parentNode.insertBefore(div4, labelsVDD[2])
+                div4.append(p4)
+                p4.innerText = `Parabéns você acertou!, Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quisquam ut illum. Reprehenderit ea vero, non delectus eius voluptatum ipsum, est natus ipsam eos laborum quia maiores vel dolores aspernatur.`
+                }
+
+                // SEGUNDA QUESTÃO DO QUIZ ||
+
+         // Verificando novamente se estão com os valores errados //
+
+        if(inputsPergunta1.value ==  'errado4' || inputsPergunta1.value ==  'errado5' || inputsPergunta1.value ==  'errado6' || inputsPergunta1.value ==  'errado7')
+        {
+            // aumentando apenas 1 questão caso seja errado 
+            questoes += 1
+    
+                            // Mesmo padrão //
+            if(inputsPergunta1.value == 'errado4')
+            { 
+                            // Mesmo padrão //
+                labelsVDD[5].parentNode.insertBefore(div5, labelsVDD[6]) 
+                div5.append(p5) 
+                p5.innerText = `a Você errou!!!`
+
+                labelsVDD[6].parentNode.insertBefore(div6, labelsVDD[7])
+                div6.append(p6)
+                p6.innerText = `Essa também não está certa por causa disso disso e disso...`
+
+                labelsVDD[7].parentNode.insertBefore(div7, labelsVDD[8])
+                div7.append(p7)
+                p7.innerText = `Essa era a resposta certa, porque...  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quisquam ut illum. Reprehenderit ea vero, non delectus eius voluptatum ipsum, est natus ipsam eos laborum quia maiores vel dolores aspernatur.`
+
+                labelsVDD[8].parentNode.insertBefore(div8, labelsVDD[9])
+                div8.append(p8)
+                p8.innerText = `Essa também não está certa por causa disso disso e disso...`
+
+                labelsVDD[9].parentNode.insertBefore(div9, contato1)
+                div9.append(p9)
+                p9.innerText = `Essa também não está certa por causa disso disso e disso...`
+
+                
+            }
+
+            else if(inputsPergunta1.value == 'errado5')
+            { // Se for o errado 1, vai acontecer isso...
+    
+                labelsVDD[5].parentNode.insertBefore(div5, labelsVDD[6]) 
+                div5.append(p5) // chamando a label 1, e inserindo a div antes da label 2
+                p5.innerText = `Essa também não está certa por causa disso disso e disso...`// e esse texto, segue igual para todas as outras...
+
+                labelsVDD[6].parentNode.insertBefore(div6, labelsVDD[7])
+                div6.append(p6)
+                p6.innerText = `a Você errou`
+
+                labelsVDD[7].parentNode.insertBefore(div7, labelsVDD[8])
+                div7.append(p7)
+                p7.innerText = `Essa era a resposta certa, porque...  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quisquam ut illum. Reprehenderit ea vero, non delectus eius voluptatum ipsum, est natus ipsam eos laborum quia maiores vel dolores aspernatur.`
+
+                labelsVDD[8].parentNode.insertBefore(div8, labelsVDD[9])
+                div8.append(p8)
+                p8.innerText = `Essa também não está certa por causa disso disso e disso...`
+
+                labelsVDD[9].parentNode.insertBefore(div9, contato1)
+                div9.append(p9)
+                p9.innerText = `Essa também não está certa por causa disso disso e disso...`
+
+                
+            }
+
+            else if(inputsPergunta1.value == 'errado6'){
+    
+                labelsVDD[5].parentNode.insertBefore(div5, labelsVDD[6]) 
+                div5.append(p5) // chamando a label 1, e inserindo a div antes da label 2
+                p5.innerText = `Essa também não está certa por causa disso disso e disso...`// e esse texto, segue igual para todas as outras...
+
+                labelsVDD[6].parentNode.insertBefore(div6, labelsVDD[7])
+                div6.append(p6)
+                p6.innerText = `Essa também não está certa por causa disso disso e disso...`
+
+                labelsVDD[7].parentNode.insertBefore(div7, labelsVDD[8])
+                div7.append(p7)
+                p7.innerText = `Essa era a resposta certa, porque...  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quisquam ut illum. Reprehenderit ea vero, non delectus eius voluptatum ipsum, est natus ipsam eos laborum quia maiores vel dolores aspernatur.`
+
+                labelsVDD[8].parentNode.insertBefore(div8, labelsVDD[9])
+                div8.append(p8)
+                p8.innerText = `Você errou!`
+
+                labelsVDD[9].parentNode.insertBefore(div9, contato1)
+                div9.append(p9)
+                p9.innerText = `Essa também não está certa por causa disso disso e disso...`
+
+                
+            }
+
+            else if(inputsPergunta1.value == 'errado7')
+            {
+                labelsVDD[5].parentNode.insertBefore(div5, labelsVDD[6]) 
+                div5.append(p5) // chamando a label 1, e inserindo a div antes da label 2
+                p5.innerText = `Essa também não está certa por causa disso disso e disso...`// e esse texto, segue igual para todas as outras...
+
+                labelsVDD[6].parentNode.insertBefore(div6, labelsVDD[7])
+                div6.append(p6)
+                p6.innerText = `Essa também não está certa por causa disso disso e disso...`
+
+                labelsVDD[7].parentNode.insertBefore(div7, labelsVDD[8])
+                div7.append(p7)
+                p7.innerText = `Essa era a resposta certa, porque...  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quisquam ut illum. Reprehenderit ea vero, non delectus eius voluptatum ipsum, est natus ipsam eos laborum quia maiores vel dolores aspernatur.`
+
+                labelsVDD[8].parentNode.insertBefore(div8, labelsVDD[9])
+                div8.append(p8)
+                p8.innerText = `Essa também não está certa por causa disso disso e disso...`
+
+                labelsVDD[9].parentNode.insertBefore(div9, contato1)
+                div9.append(p9)
+                p9.innerText = `Você errou!!`            
+            }
+        }
+
+        else
+        {
+            // Mesma coisa pra quando a segunda questão está certa //
+            questoes += 1
+            acertos += 1
+
+                        // Mesmo padrão //
+            labelsVDD[5].parentNode.insertBefore(div5, labelsVDD[6]) 
+            div5.append(p5) // chamando a label 1, e inserindo a div antes da label 2
+            p5.innerText = `Essa resposta está errada por causa disso disso e disso..`// e esse texto, segue igual para todas as outras...
+
+            labelsVDD[6].parentNode.insertBefore(div6, labelsVDD[7])
+            div6.append(p6)
+            p6.innerText = `Essa resposta está errada por causa disso disso e disso..`
+
+            labelsVDD[7].parentNode.insertBefore(div7, labelsVDD[8])
+            div7.append(p7)
+            p7.innerText = `Parabéns você acertou! essa é a resposta certa porque...  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quisquam ut illum. Reprehenderit ea vero, non delectus eius voluptatum ipsum, est natus ipsam eos laborum quia maiores vel dolores aspernatur.`
+
+            labelsVDD[8].parentNode.insertBefore(div8, labelsVDD[9])
+            div8.append(p8)
+            p8.innerText = `Essa resposta está errada por causa disso disso e disso..`
+
+            labelsVDD[9].parentNode.insertBefore(div9, contato1)
+            div9.append(p9)
+            p9.innerText = `Essa resposta está errada por causa disso disso e disso...`
+            } 
+
+
+            // No fim criamos a parte de resultado do quiz//
+
+            // Um novo elemento que entra na div resultado-do-quiz //
+            let divResultado = document.createElement('div')
+            divResultado.classList.add('resultado-quiz')
+            let h4Resultado = document.createElement('h4')
+            h4Resultado.innerText = `Você acertou ${acertos} de ${questoes} questões!`
+            
+            divResultado.append(h4Resultado)
+            resultado.append(divResultado)
              }
         }
-    }
-
+    
